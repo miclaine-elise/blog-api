1b3b781b801c3e95af911899adbd431b9300eebc
Miclaine Emtman <clainie@gmail.com>
2024-06-20 15:16:23 -0700

    Fixing .env file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const db = require("./mongoose");
 
 const app = express();
 db.connectDB();
+// const corsOptions = {
+//     origin: [process.env.PUBLIC_URL, process.env.PRIVATE_URL],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,23 +27,6 @@ app.use((req, res, next) => {
 });
 app.use('/blog', routes.blog);
 app.use('/admin', routes.admin);
-
-
-app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
-});
-
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
-
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-});
 
 app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`),
