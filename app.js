66646b3e156b688cfea22b38e3aa0f9adde0e77e
Miclaine Emtman <clainie@gmail.com>
2024-06-20 23:23:55 -0700

    Delete fly.io files and try Railway

--- app.js
+++ app.js
@@ -4,32 +4,53 @@ const express = require("express");
 const models = require('./models/post');
 const apiPublicRouter = require("./routes/api-public");
 const apiPrivateRouter = require("./routes/api-private");
-
-// import models from './models/post';
-// import routes from './routes';
 const db = require("./mongoose");
 
 const app = express();
 db.connectDB();
-// const corsOptions = {
-//     origin: ['https://soft-fairy-21b84f.netlify.app'],
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-    req.context = {
-        models,
-    };
-    next();
-});
+// app.use((req, res, next) => {
+//     req.context = {
+//         models,
+//     };
+//     next();
+// });
 app.use('/blog', apiPublicRouter);
 app.use('/admin', apiPrivateRouter);
+// home route
+app.get("/", (req, res, next) => {
+    res.status(200).json({ message: "welcome to the blog home page!" });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set error and provide in development
+    const message = err.message;
+    const error = req.app.get("env") === "development" ? err : {};
+
+    // return error JSON
+    res.status(err.status || 500).json({
+        error: {
+            message,
+            status: error.status,
+            stack: error.stack,
+        },
+    });
+});
 
 // app.listen(process.env.PORT, () =>
 //     console.log(`Example app listening on port ${process.env.PORT}!`),
